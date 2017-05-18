6ef5a14d1fb453d3bc050d0e2a1f92558e62ef02
David Jones <dgjones@gmail.com>
2017-05-18 16:08:21 -0700

    disable the silly rspec example length rubocop

--- spec/lib/percy/capybara/loaders/ember_cli_rails_loader_spec.rb
+++ spec/lib/percy/capybara/loaders/ember_cli_rails_loader_spec.rb
@@ -61,8 +61,11 @@ RSpec.describe Percy::Capybara::Loaders::EmberCliRailsLoader do
 
         it 'builds the expected resources' do
           expected_urls = loader.build_resources.collect(&:resource_url)
-          expected_url  = loader._uri_join(mount_path, described_class::EMBER_ASSETS_DIR,
-            "percy-#{ember_app}.svg",)
+          expected_url = loader._uri_join(
+            mount_path,
+            described_class::EMBER_ASSETS_DIR,
+            "percy-#{ember_app}.svg",
+          )
 
           expect(expected_urls).to include(expected_url)
         end
