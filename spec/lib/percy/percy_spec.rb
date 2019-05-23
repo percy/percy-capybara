0eab85c77f258a087485266a62000c7be0296189
Robert DeLuca <robertdeluca19@gmail.com>
2019-05-23 10:51:20 -0500

    Remove file encoding emacs inserted

--- spec/lib/percy/percy_spec.rb
+++ spec/lib/percy/percy_spec.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
 RSpec.describe Percy, type: :feature do
   TEST_CASE_GLOB =  File.join(File.dirname(__FILE__), "./capybara/client/test_data/test-*.html")
 
@@ -40,7 +39,7 @@ RSpec.describe Percy, type: :feature do
         visit 'http://example.com'
         Percy.snapshot(page, { name: 'minHeight', minHeight: 2000 })
       end
-      it 'recognizes enableJavascript' do
+      it 'recognizes enable_javascript' do
         visit 'http://example.com'
         Percy.snapshot(page, { name: 'enableJavaScript', enable_javascript: true })
       end
